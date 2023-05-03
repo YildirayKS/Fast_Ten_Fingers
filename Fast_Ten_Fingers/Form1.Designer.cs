48d3dd4d5d76b4f1fbc83b52b64a7c24c657c3f7
YildirayKS <yildiray_karasubasi20@erdogan.edu.tr>
2023-05-03 11:14:34 +0300

    Files created & sent

---
+++ Fast_Ten_Fingers/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Fast_Ten_Fingers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtbox_words = new System.Windows.Forms.TextBox();
+            this.lbl_word = new System.Windows.Forms.Label();
+            this.lbl_point = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_start = new System.Windows.Forms.Button();
+            this.lbl_yourpoint = new System.Windows.Forms.Label();
+            this.lbl_true = new System.Windows.Forms.Label();
+            this.lbl_false = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtbox_words
+            // 
+            this.txtbox_words.Location = new System.Drawing.Point(12, 276);
+            this.txtbox_words.Name = "txtbox_words";
+            this.txtbox_words.Size = new System.Drawing.Size(403, 30);
+            this.txtbox_words.TabIndex = 1;
+            this.txtbox_words.Visible = false;
+            this.txtbox_words.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // lbl_word
+            // 
+            this.lbl_word.AutoSize = true;
+            this.lbl_word.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_word.Location = new System.Drawing.Point(155, 110);
+            this.lbl_word.Name = "lbl_word";
+            this.lbl_word.Size = new System.Drawing.Size(116, 46);
+            this.lbl_word.TabIndex = 2;
+            this.lbl_word.Text = "Word";
+            this.lbl_word.Visible = false;
+            // 
+            // lbl_point
+            // 
+            this.lbl_point.AutoSize = true;
+            this.lbl_point.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_point.Location = new System.Drawing.Point(314, 9);
+            this.lbl_point.Name = "lbl_point";
+            this.lbl_point.Size = new System.Drawing.Size(74, 29);
+            this.lbl_point.TabIndex = 3;
+            this.lbl_point.Text = "Point:";
+            this.lbl_point.Visible = false;
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_time.Location = new System.Drawing.Point(7, 9);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(107, 29);
+            this.lbl_time.TabIndex = 4;
+            this.lbl_time.Text = "Time: 60";
+            this.lbl_time.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(133, 186);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(159, 62);
+            this.btn_start.TabIndex = 5;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_yourpoint
+            // 
+            this.lbl_yourpoint.AutoSize = true;
+            this.lbl_yourpoint.Location = new System.Drawing.Point(158, 76);
+            this.lbl_yourpoint.Name = "lbl_yourpoint";
+            this.lbl_yourpoint.Size = new System.Drawing.Size(108, 25);
+            this.lbl_yourpoint.TabIndex = 6;
+            this.lbl_yourpoint.Text = "Your Point:";
+            this.lbl_yourpoint.Visible = false;
+            // 
+            // lbl_true
+            // 
+            this.lbl_true.AutoSize = true;
+            this.lbl_true.ForeColor = System.Drawing.Color.LawnGreen;
+            this.lbl_true.Location = new System.Drawing.Point(23, 336);
+            this.lbl_true.Name = "lbl_true";
+            this.lbl_true.Size = new System.Drawing.Size(59, 25);
+            this.lbl_true.TabIndex = 7;
+            this.lbl_true.Text = "True:";
+            this.lbl_true.Visible = false;
+            // 
+            // lbl_false
+            // 
+            this.lbl_false.AutoSize = true;
+            this.lbl_false.BackColor = System.Drawing.Color.DimGray;
+            this.lbl_false.ForeColor = System.Drawing.Color.Red;
+            this.lbl_false.Location = new System.Drawing.Point(286, 336);
+            this.lbl_false.Name = "lbl_false";
+            this.lbl_false.Size = new System.Drawing.Size(66, 25);
+            this.lbl_false.TabIndex = 7;
+            this.lbl_false.Text = "False:";
+            this.lbl_false.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(427, 385);
+            this.Controls.Add(this.lbl_false);
+            this.Controls.Add(this.lbl_true);
+            this.Controls.Add(this.lbl_yourpoint);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_point);
+            this.Controls.Add(this.lbl_word);
+            this.Controls.Add(this.txtbox_words);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fast Ten Fingers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbox_words;
+        private System.Windows.Forms.Label lbl_word;
+        private System.Windows.Forms.Label lbl_point;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Label lbl_yourpoint;
+        private System.Windows.Forms.Label lbl_true;
+        private System.Windows.Forms.Label lbl_false;
+    }
+}
+
---
+++ Fast_Ten_Fingers/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace Fast_Ten_Fingers
+{
+    public partial class Form1 : Form
+    {
+        int time = 60;
+        int point = 0;
+        int trueCount, falseCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtbox_words.Text.ToLower() == lbl_word.Text)
+                {
+                    trueCount++;
+                    point++;
+                    lbl_point.Text = "Point: " + point;
+                    txtbox_words.Clear();
+                    randomWord();
+                }
+                else
+                {
+                    falseCount++;
+                    txtbox_words.Clear();
+                    randomWord();
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (time > 0)
+            {
+                time--;
+                lbl_time.Text = "Time: " + time;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btn_start.Visible = true;
+                hideAll();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            randomWord();
+
+            point = 0;
+            trueCount = 0;
+            falseCount = 0;
+
+            lbl_point.Text = "Point: " + point;
+            lbl_time.Text = "Time: 60";
+
+            lbl_time.Visible = true;
+            lbl_point.Visible = true;
+            lbl_word.Visible = true;
+
+            lbl_yourpoint.Visible = false;
+            lbl_true.Visible = false;
+            lbl_false.Visible = false;
+
+            txtbox_words.Visible = true;
+
+            btn_start.Visible = false;
+        }
+
+        public void hideAll()
+        {
+            time = 60;
+            lbl_time.Visible = false;
+            lbl_point.Visible = false;
+            lbl_word.Visible = false;
+
+            lbl_yourpoint.Visible = true;
+            lbl_true.Visible = true;
+            lbl_false.Visible = true;
+
+            txtbox_words.Visible = false;
+
+            lbl_yourpoint.Text = "Your Point: " + point;
+            lbl_true.Text = "True: " + trueCount;
+            lbl_false.Text = "False: " + falseCount;
+        }
+
+        public void randomWord()
+        {
+            Random randomWord = new Random();
+
+            string filePath = @"C:Words.txt";
+
+            string[] words = File.ReadAllLines(filePath);
+
+            Random random = new Random();
+            string word = words[random.Next(words.Length)];
+
+            lbl_word.Text = word;
+        }
+    }
+}
